bfe974e76a406c50d621556b9f7282f19ccd7767
Jarad Comingdeer <jarad@jcomingdeer.com>
2014-03-26 21:04:09 -0700

    Replace back and foward slider buttons with individual slide buttons

--- js/jaradcomingdeer.js
+++ js/jaradcomingdeer.js
@@ -12,18 +12,8 @@ var JaradApp = function(){
 		init: function() {
 			slider = $('#projects').bxSlider({
 				controls: false,
-			  	pager: true,
-			  	pagerType: "short"
-			});
-			
-			$('#projects-window a.button.previous').click(function(){
-			  	slider.goToPreviousSlide();
-			  	return false;
-			});
-			
-			$('#projects-window a.button.next').click(function(){
-			  	slider.goToNextSlide();
-			  	return false;
+				pager: true,
+				pagerType: "full"
 			});
 			
 			// debounce resizing listener for performance
