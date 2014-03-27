b14d9db20e9f3ed4afc43bfcb828c49524687264
Jarad Comingdeer <jarad@jcomingdeer.com>
2014-03-27 13:58:30 -0700

    Moar little jarads!

--- js/jaradcomingdeer.js
+++ js/jaradcomingdeer.js
@@ -1,23 +1,39 @@
 var JaradApp = function(){
 	var slider;
+	var currentJarad = 0;
+	var jarads = [];
 
 	function resizeSlider() {
 		var slide = slider.getCurrentSlide();
 		slider.reloadShow();
 		slider.goToSlide(slide);
-	};
-	
+	}
+
+	function swapJarads() {
+		$(jarads[currentJarad]).toggleClass('active');
+		currentJarad = (currentJarad+1)%jarads.length;
+		$(jarads[currentJarad]).toggleClass('active');
+	}
+
+	function init() {
+		slider = $('#projects').bxSlider({
+			controls: false,
+			pager: true,
+			pagerType: "full"
+		});
+
+		$('#little-jarads').children('img').each(function () {
+			jarads.push(this);
+		});
+
+		setInterval(swapJarads, 6000);
+
+		// debounce resizing listener for performance
+		$(window).resize($.debounce(500, resizeSlider));
+	}
+
 	// Probably didn't need to module this but wth, why not.
 	return {
-		init: function() {
-			slider = $('#projects').bxSlider({
-				controls: false,
-				pager: true,
-				pagerType: "full"
-			});
-			
-			// debounce resizing listener for performance
-			$(window).resize($.debounce(500, resizeSlider));
-		}
-	}
+		init: init
+	};
 }();
